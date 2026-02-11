10825079b29039d413e0d2b4bd85487437de97cb
Nitin-sounkaria <nitin.bvcoe2024@gmail.com>
2026-02-11 17:14:26 +0530

    Refactor: Expand size guide to individual EU size rows (13-33)

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,12 +1,3 @@
-export const SIZE_GUIDE_DATA = [
-    { ourSize: 'XS', age: '0 - 9 Months', eu: '13 - 18', inch: '3.5" - 4.125"', cm: '8.9 - 10.5' },
-    { ourSize: 'S', age: '10 Months - 2 Years', eu: '19 - 22', inch: '4.5" - 5.125"', cm: '11.4 - 13' },
-    { ourSize: 'M', age: '2 - 4 Years', eu: '23 - 25', inch: '5.5" - 6.125"', cm: '14 - 15.6' },
-    { ourSize: 'L', age: '4 - 5 Years', eu: '26 - 28', inch: '6.25" - 6.75"', cm: '15.9 - 17.1' },
-    { ourSize: 'XL', age: '5 - 6 Years', eu: '29 - 31', inch: '6.8" - 7.5"', cm: '17.5 - 19.1' },
-    { ourSize: 'XXL', age: '7 - 8 Years', eu: '32 - 33', inch: '7.75" - 8.125"', cm: '19.7 - 20.6' },
-];
-
 export const SIZE_LABELS = {
     XS: '1-9 months',
     S: '10 months - 2 years',
@@ -15,3 +6,38 @@ export const SIZE_LABELS = {
     XL: '5-6 years',
     XXL: '7-8 years',
 };
+
+export const SIZE_GUIDE_DATA = [
+    // XS Group
+    { ourSize: 'XS', age: '1-3 Months', eu: '13', inch: '3.5"', cm: '8.9' },
+    { ourSize: 'XS', age: '1-3 Months', eu: '14', inch: '3.625"', cm: '9.2' },
+    { ourSize: 'XS', age: '3-6 Months', eu: '15', inch: '3.75"', cm: '9.5' },
+    { ourSize: 'XS', age: '3-6 Months', eu: '16', inch: '3.875"', cm: '9.8' },
+    { ourSize: 'XS', age: '6-9 Months', eu: '17', inch: '4.125"', cm: '10.5' },
+    { ourSize: 'XS', age: '6-9 Months', eu: '18', inch: '4.25"', cm: '10.8' },
+
+    // S Group
+    { ourSize: 'S', age: '10-12 Months', eu: '19', inch: '4.5"', cm: '11.4' },
+    { ourSize: 'S', age: '12-15 Months', eu: '20', inch: '4.75"', cm: '12.1' },
+    { ourSize: 'S', age: '15-18 Months', eu: '21', inch: '4.875"', cm: '12.4' },
+    { ourSize: 'S', age: '1.5-2 Years', eu: '22', inch: '5.125"', cm: '13' },
+
+    // M Group
+    { ourSize: 'M', age: '2-2.5 Years', eu: '23', inch: '5.5"', cm: '14' },
+    { ourSize: 'M', age: '2.5-3 Years', eu: '24', inch: '5.75"', cm: '14.6' },
+    { ourSize: 'M', age: '3-4 Years', eu: '25', inch: '6.125"', cm: '15.6' },
+
+    // L Group
+    { ourSize: 'L', age: '4-4.5 Years', eu: '26', inch: '6.25"', cm: '15.9' },
+    { ourSize: 'L', age: '4.5-5 Years', eu: '27', inch: '6.5"', cm: '16.5' },
+    { ourSize: 'L', age: '5 Years', eu: '28', inch: '6.75"', cm: '17.1' },
+
+    // XL Group
+    { ourSize: 'XL', age: '5-5.5 Years', eu: '29', inch: '6.875"', cm: '17.5' },
+    { ourSize: 'XL', age: '5.5-6 Years', eu: '30', inch: '7.125"', cm: '18.1' },
+    { ourSize: 'XL', age: '6 Years', eu: '31', inch: '7.5"', cm: '19.1' },
+
+    // XXL Group
+    { ourSize: 'XXL', age: '7 Years', eu: '32', inch: '7.75"', cm: '19.7' },
+    { ourSize: 'XXL', age: '8 Years', eu: '33', inch: '8.125"', cm: '20.6' },
+];
